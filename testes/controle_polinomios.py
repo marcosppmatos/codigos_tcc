c12b526ad9cd874c9f9145c68103ce5cc92b8a2e
Marcos <themarcos1996@gmail.com>
2023-07-15 11:20:08 -0300

    nao deu certo o teste pra novos polinomios

--- testes/controle_polinomios.py
+++ testes/controle_polinomios.py
@@ -1,6 +1,7 @@
 from auxiliar import bd_polinomios
 from deterministico import algoritmo_deterministico
 from monte_carlo import algoritmo_monte_carlo
+import random
 
 
 def aux():
@@ -9,5 +10,26 @@ def aux():
         print(algoritmo_monte_carlo(p1, p2, p3, 5))
         print(algoritmo_deterministico(p1, p2, p3))
         print('***************************************************************************************')
-
 aux()
+
+
+def main():
+    def generate_polynomial(grau):
+        coeficientes = [random.randint(-10, 10) for _ in range(grau + 1)]
+        polinomio = ""
+        for i in range(grau, -1, -1):
+            coeficiente = coeficientes[grau - i]
+            if coeficiente != 0:
+                polinomio += f"{coeficiente}x^({i} + "
+        polinomio = polinomio.rstrip(" + ")
+        return polinomio
+
+    polinomios = []
+    for _ in range(20):
+        grau = random.randint(7, 10)
+        polinomio = generate_polynomial(grau)
+        polinomios.append(polinomio)
+
+    for polinomio in polinomios:
+        print(polinomio)
+# main()
